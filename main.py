af04259f773c5099761cc9d15d9f60903dce177f
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-03-27 22:57:27 -0300

    Código adicionado

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+#Pedindo a informação do usuário
+raio = float(input("Digite o raio do círculo: "))
+
+#Calculando a área do círculo
+operação = 3.14 * (raio * raio)
+operação = float(operação)
+
+#Printando o valor para o usuário
+print("A área do círculo é " + str(operação) + " m².")
 
